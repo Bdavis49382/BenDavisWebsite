02cb32424f5cae20354a3ae56ae07819788883c4
Ben Davis <bendavis493@gmail.com>
2025-03-12 20:47:35 -0600

    added video upload capability and safety so admin page is only viewable in dev mode.

---
+++ wwwroot/playVideo.js
@@ -0,0 +1,11 @@
+﻿function setVideoAttributes() {
+    const videos = document.querySelectorAll('.video');
+    console.log(videos);
+    videos.forEach(video => {
+        video.autoplay = true;
+        video.muted = true;
+        video.play();
+        console.log('video found');
+    })
+}
+
